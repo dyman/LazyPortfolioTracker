36173cf07cfaacd0c9d250ceac2c1095c92ef17c
dyman <mandypeter@gmail.com>
2015-12-11 10:21:04 +0100

    Update 1.sql

--- LPT/conf/evolutions/default/1.sql
+++ LPT/conf/evolutions/default/1.sql
@@ -7,10 +7,39 @@ CREATE TABLE currency
 );
 
 
-INSERT INTO currency (id, name) VALUES ('HUF', 'Forint');
-INSERT INTO currency (id, name) VALUES ('USD', 'Dollar');
-INSERT INTO currency (id, name) VALUES ('EUR', 'Euro');
-INSERT INTO currency (id, name) VALUES ('JPY', 'Yen');
+INSERT INTO currency (id, name) VALUES ('USD', 'US dollar');
+INSERT INTO currency (id, name) VALUES ('JPY', 'Japanese yen');
+INSERT INTO currency (id, name) VALUES ('BGN', 'Bulgarian lev');
+INSERT INTO currency (id, name) VALUES ('CZK', 'Czech koruna');
+INSERT INTO currency (id, name) VALUES ('DKK', 'Danish krone');
+INSERT INTO currency (id, name) VALUES ('GBP', 'Pound sterling');
+INSERT INTO currency (id, name) VALUES ('HUF', 'Hungarian forint');
+INSERT INTO currency (id, name) VALUES ('PLN', 'Polish zloty');
+INSERT INTO currency (id, name) VALUES ('RON', 'Romanian leu');
+INSERT INTO currency (id, name) VALUES ('SEK', 'Swedish krona');
+INSERT INTO currency (id, name) VALUES ('CHF', 'Swiss franc');
+INSERT INTO currency (id, name) VALUES ('NOK', 'Norwegian krone');
+INSERT INTO currency (id, name) VALUES ('HRK', 'Croatian kuna');
+INSERT INTO currency (id, name) VALUES ('RUB', 'Russian rouble');
+INSERT INTO currency (id, name) VALUES ('TRY', 'Turkish lira');
+INSERT INTO currency (id, name) VALUES ('AUD', 'Australian dollar');
+INSERT INTO currency (id, name) VALUES ('BRL', 'Brazilian real');
+INSERT INTO currency (id, name) VALUES ('CAD', 'Canadian dollar');
+INSERT INTO currency (id, name) VALUES ('CNY', 'Chinese yuan renminb');
+INSERT INTO currency (id, name) VALUES ('HKD', 'Hong Kong dollar');
+INSERT INTO currency (id, name) VALUES ('IDR', 'Indonesian rupiah');
+INSERT INTO currency (id, name) VALUES ('ILS', 'Israeli shekel');
+INSERT INTO currency (id, name) VALUES ('INR', 'Indian rupee');
+INSERT INTO currency (id, name) VALUES ('KRW', 'South Korean won');
+INSERT INTO currency (id, name) VALUES ('MXN', 'Mexican peso');
+INSERT INTO currency (id, name) VALUES ('MYR', 'Malaysian ringgit');
+INSERT INTO currency (id, name) VALUES ('NZD', 'New Zealand dollar');
+INSERT INTO currency (id, name) VALUES ('PHP', 'Philippine peso');
+INSERT INTO currency (id, name) VALUES ('SGD', 'Singapore dollar');
+INSERT INTO currency (id, name) VALUES ('THB', 'Thai baht');
+INSERT INTO currency (id, name) VALUES ('ZAR', 'South African rand');
+INSERT INTO currency (id, name) VALUES ('ISK', 'Icelandic krona ');
+
 
 
 CREATE TABLE "country"
